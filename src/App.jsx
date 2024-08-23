7a12fb48e64ca578bb5d0e46b7dc22453c550af7
victor <prkone@hotmail.fr>
2024-08-23 18:59:36 +0200

    fix useeffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import startSound from './assets/sound/smooth.mp3';
 import endSound from './assets/sound/sharp.mp3';
